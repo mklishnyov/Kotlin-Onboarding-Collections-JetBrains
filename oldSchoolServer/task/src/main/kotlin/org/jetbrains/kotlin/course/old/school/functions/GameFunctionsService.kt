0273363dce7438ed956ea28df5b19af85a8b8e12
Matvey <klishnev2018@gmail.com>
2025-09-12 17:26:20 +0500

    12.09.25 v2

--- oldSchoolServer/task/src/main/kotlin/org/jetbrains/kotlin/course/old/school/functions/GameFunctionsService.kt
+++ oldSchoolServer/task/src/main/kotlin/org/jetbrains/kotlin/course/old/school/functions/GameFunctionsService.kt
@@ -1,6 +1,8 @@
 package org.jetbrains.kotlin.course.old.school.functions
 
+import org.jetbrains.kotlin.course.old.school.photo.Accessory
 import org.jetbrains.kotlin.course.old.school.photo.Color
+import org.jetbrains.kotlin.course.old.school.photo.HairType
 import org.jetbrains.kotlin.course.old.school.photo.PhotoCharacter
 import org.springframework.stereotype.Service
 
@@ -21,5 +23,11 @@ class GameFunctionsService {
     fun Iterable<String>.groupPhotosByColor(): List<PhotoCharacter> = this.toPhotoCharacters()
         .groupBy { b -> b.backgroundColor.name }.flatMap { it.value }
 
-    fun Iterable<String>.groupPhotosByHairAndHat(): List<PhotoCharacter> = TODO("Not implemented yet")
+    fun Iterable<String>.groupPhotosByHairAndHat(): List<PhotoCharacter> {
+        val groupedByHair =  this.toPhotoCharacters()
+            .groupBy { it.hairType }
+        val darkHair = groupedByHair[HairType.Dark].groupBy { it.accessories?.contains(Accessory.Hat) }.flatMap { it.value }
+        val lightHair = groupedByHair[HairType.Light].groupBy { it.accessories?.contains(Accessory.Hat) }.flatMap { it.value }
+        return darkHair + lightHair
+    }
 }
