f30bc2853114941279ebffbb1435c2b9d16810cb
Matvey <klishnev2018@gmail.com>
2025-09-17 10:35:59 +0500

    17.09.25

--- culinaryServer/task/src/main/kotlin/org/jetbrains/kotlin/course/culinary/game/recipes/Smoothie.kt
+++ culinaryServer/task/src/main/kotlin/org/jetbrains/kotlin/course/culinary/game/recipes/Smoothie.kt
@@ -1,11 +1,21 @@
 package org.jetbrains.kotlin.course.culinary.game.recipes
 
+import org.jetbrains.kotlin.course.culinary.game.blender
 import org.jetbrains.kotlin.course.culinary.game.fridge
+import org.jetbrains.kotlin.course.culinary.game.kitchen
 import org.jetbrains.kotlin.course.culinary.models.food.Fruit
 import org.jetbrains.kotlin.course.culinary.models.food.FruitType
 
-fun getFruitsForSmoothie(): List<Fruit> = fridge.getBasketOf(FruitType.entries.random())
+fun getFruitsForSmoothie(): List<Fruit> =
+    listOf(FruitType.Berry, FruitType.Citrus)
+        .map { type -> fridge.getBasketOf(type).also { basket ->
+            kitchen.put(basket)
+            kitchen.takeFromBasket(basket)
+        }}
+        .flatMap { basket -> List(basket.capacity) { Fruit(basket.type) } }
+        .sortedBy { it.type.sugarContent }
 
 fun List<Fruit>.cookSmoothie() {
-    TODO("Not implemented yet")
+    this.forEach { blender.add(it) }
+    blender.blend()
 }
